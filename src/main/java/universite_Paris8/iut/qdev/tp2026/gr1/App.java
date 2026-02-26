2145767be6038d868a31880116b219d1426450f1
Denis ZAPOI <deniszapoi.pro@gmail.com>
2026-02-26 12:23:01 +0100

    commit initial

---
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr1/App.java
@@ -0,0 +1,10 @@
+package universite_Paris8.iut.qdev.tp2026.gr1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/universite_Paris8/iut/qdev/tp2026/gr1/AppTest.java
@@ -0,0 +1,19 @@
+package universite_Paris8.iut.qdev.tp2026.gr1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
